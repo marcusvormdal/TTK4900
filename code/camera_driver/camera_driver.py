1ebf2fe2bd9725da48c9a1937ba1abeca8e834e7
marcusvormdal <marcusvormdal@gmail.com>
2023-04-12 20:16:12 +0200

    camera detect works (pray)

--- code/camera_driver/camera_driver.py
+++ code/camera_driver/camera_driver.py
@@ -16,8 +16,10 @@ def get_camera_frame(video, start_stamp, video_path):
     m = timedelta(minutes=(int(float(duration)/60))-60)
     s = timedelta(seconds=(int(float(duration)%60)+3))
     start_time = end_time - m - s
-    stamp = datetime.timestamp(start_time) - 0.25
-    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    stamp = datetime.timestamp(start_time) - 0.25 
+    video_offset = int((start_stamp - stamp) * 4 - 15)
+    stamp = stamp + int(start_stamp - stamp) -15
+    video.set(cv2.CAP_PROP_POS_FRAMES, video_offset)
     success = True
     for i in range(frame_num):
         success, img = video.read()
@@ -60,7 +62,7 @@ def calculate_angle(bbox):
 
 
 def get_world_coordinate(psi,theta,phi, t1,t2,t3, box_coord):
-    f = 120
+    f = 2.8
     ox = 1344
     oy = 760
     
@@ -74,15 +76,36 @@ def get_world_coordinate(psi,theta,phi, t1,t2,t3, box_coord):
                     [0,np.cos(phi), -np.sin(phi)],
                     [0,np.sin(phi), np.cos(phi)]])
     
+    R_bc = (R_z@R_y@R_x)
+    R_bc_inv = np.linalg.inv(R_bc).round(5)
     R = np.zeros([3,3])
-    R[:, 0:2] = (R_z@R_y@R_x)[:, 0:2]
-    R[2,0], R[2,1], R[2,2] = t1, t2, t3
-    #print(R)
     
+    R[0:2, 0:2] = R_bc[0:2, 0:2]
+
+    test = [t1,t2,t3]
+    test = -R_bc_inv @ test 
+    
+    print(test)
+    
+    R[2,0], R[2,1], R[2,2] = test[0], test[1], test[2]
     i = np.array([[f, 0, ox],[0,f, oy],[0,0,1]])
-    
-    A_in = np.linalg.inv(i @ R)
+
+    print(np.linalg.pinv(R).round(5))
+    print(np.linalg.inv(i).round(5))
     hom_cord = np.array([box_coord[1], box_coord[2], 1])
-    world_cord = A_in @ hom_cord.T
+    world_cord = (np.linalg.pinv(R).round(5) @ np.linalg.inv(i).round(5) @ hom_cord.T).round(2) / 1.59
+    
     print("Wcord:",world_cord, np.arctan2(world_cord[1], world_cord[0])*180/np.pi)
     return world_cord
+
+def test_world_coord():
+    psi,theta,phi = (np.pi/180)*90,0,np.pi
+    t1,t2,t3 = 0,0,0.63
+    box_coord  = (0,1344,1090)
+    
+    world_cord = get_world_coordinate(psi, theta, phi, t1, t2, t3, box_coord)
+    
+def __main__():
+    test_world_coord()
+    
+__main__()
